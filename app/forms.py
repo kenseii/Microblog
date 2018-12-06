36f0fe9cf793bdbdd553c183e9bb2905c12997b3
oasis <oasisagano@gmail.com>
2018-12-07 01:43:58 +0900

    Finished chapter 7, added a validator to omit changing username to a taken one on edit

--- app/forms.py
+++ app/forms.py
@@ -34,9 +34,20 @@ class RegistrationForm(FlaskForm):
             raise ValidationError('Please use a different email.')
 
 
-# edif profile form
+# edit profile form
 
 class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        # in case their are different validate with the database else allow to change
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('Please use a different username')
