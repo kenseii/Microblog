7a2c0251f7bc70ed98602bf2bdef709ae6eae174
oasis <oasisagano@gmail.com>
2018-12-06 16:09:11 +0900

    after finishing chapter2, added templates

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,20 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello world"
+    user = {'username': 'Oasis'}
+    posts =[
+        {
+            'author':{'username':'John'},
+            'body':'Rainy day in Kobe'
+        },
+        {
+            'author':{'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+
+    return render_template('index.html',title='Home', user=user, posts=posts)
