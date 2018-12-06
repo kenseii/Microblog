d60990ebf452249c23079a3446a8bc3459088a34
oasis <oasisagano@gmail.com>
2018-12-06 23:02:30 +0900

    Added registration and a working profile page

--- app/routes.py
+++ app/routes.py
@@ -3,8 +3,8 @@ from flask import request
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 
-from app import app
-from app.forms import LoginForm
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
 from app.models import User
 
 
@@ -58,3 +58,34 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+# register route
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    # in case the user is already authenticated dont bother
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        # create the user object with the inserted data
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
+# profile page
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
