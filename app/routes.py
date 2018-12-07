4b50f062c1d8141f315e195a8ca35615bda0ba18
oasis <oasisagano@gmail.com>
2018-12-07 13:13:01 +0900

    Finished chapter 8, added follow/unfollow on the routes and view

--- app/routes.py
+++ app/routes.py
@@ -118,3 +118,41 @@ def edit_profile():
     return render_template('edit_profile.html',
                            title='Edit Profile',
                            form=form)
+
+
+# follow route
+
+
+@app.route('/follow/<username>')
+@login_required
+def follow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('User {} not found.'.format(username))
+        return redirect(url_for('index'))
+    if user == current_user:
+        flash('You cannot follow yourself, go get some friends!')
+        return redirect(url_for('user', username=username))
+    current_user.follow(user)
+    db.session.commit()
+    flash('You are following {}!'.format(username))
+    return redirect(url_for('user', username=username))
+
+
+# Unfollow route
+
+
+@app.route('/unfollow/<username>')
+@login_required
+def unfollow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('User {} not found.'.format(username))
+        return redirect(url_for('index'))
+    if user == current_user:
+        flash('You cannot unfollow yourself LOL!')
+        return redirect(url_for('user', username=username))
+    current_user.unfollow(user)
+    db.session.commit()
+    flash('You are not following {}.'.format(username))
+    return redirect(url_for('user', username=username))
