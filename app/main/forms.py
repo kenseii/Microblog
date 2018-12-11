b94a205f33c7ca0a7e6350db91e321d6ba034e01
oasis <oasisagano@gmail.com>
2018-12-12 00:12:24 +0900

    Finished chapter 16, added elasticsearch

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,3 +1,4 @@
+from flask import request
 from flask_babel import _, lazy_gettext as _l
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
@@ -6,7 +7,18 @@ from wtforms.validators import ValidationError, DataRequired, Length
 from app.models import User
 
 
+class SearchForm(FlaskForm):
+    q = StringField(_l('Search'), validators=[DataRequired()])
+
+    def __init__(self, *args, **kwargs):
+        if 'formdata' not in kwargs:
+            kwargs['formdata'] = request.args
+        if 'csrf_enabled' not in kwargs:
+            kwargs['csrf_enabled'] = False
+        super(SearchForm, self).__init__(*args, **kwargs)
+
 # edit profile form
+
 
 class EditProfileForm(FlaskForm):
     username = StringField(_l('Username'), validators=[DataRequired()])
